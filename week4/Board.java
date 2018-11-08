03115037d9e7ea05581c139a830686da6f66df31
Roman Morozov <sublimeye.ua@gmail.com>
2018-11-07 21:07:26 -0800

    feat(init) create base interface for Board and Solver classes

---
+++ week4/Board.java
@@ -0,0 +1,63 @@
+/* *****************************************************************************
+ *  Name: Roman Morozov
+ *  Date: Nov 7 2018
+ *  Description: Board class for 8-pieces Puzzle
+ **************************************************************************** */
+
+public class Board {
+    private int n;
+    private Board twin;
+
+    // construct a board from an n-by-n array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+    public Board(int[][] blocks) {
+        if (blocks == null) {
+            throw new IllegalArgumentException("cant be null");
+        }
+        this.n = blocks[0].length;
+        this.twin = new Board(blocks);
+    }
+
+    // board dimension n
+    public int dimensions() {
+        return this.n;
+    }
+
+    // number of blocks out of place
+    public int hamming() {
+        return 0;
+    }
+
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+        return 0;
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+        return false;
+    }
+
+    // a board that is obtained by exchanging any pair of blocks
+    public Board twin() {
+        return this.twin;
+    }
+
+    // does this board equal y?
+    public boolean equals(Object y) {
+        return false;
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+    }
+
+    // string representation of this board (in the output format specified below)
+    public String toString() {
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ week4/Solver.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Solver {
+    public Solver(Board initial) {
+    }
+
+    public boolean isSolvable() {
+        return false;
+    }
+
+    public int moves() {
+        return 0;
+    }
+
+    public Iterable<Board> solution() {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
